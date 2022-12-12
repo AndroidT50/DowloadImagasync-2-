4b11878bb88f664ba29e98ad1047db5530bd83af
Vint k <vintkorp@outlook.com>
2022-12-12 22:50:08 +0700

    Итог

---
+++ DowloadImagasync(2)/ImageDownloader.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace ConsoleApp19
+{
+    public class ImageDownloader
+    {
+        public Task task;
+        public event Action<string> ImageStarted;
+        public event Action<string> ImageCompleted;
+        public void Download(string remoteUri, string fileName)
+        {
+            ImageStarted?.Invoke(fileName);
+            using (WebClient myWebClient = new WebClient())
+            {
+                myWebClient.DownloadFileCompleted += (s, e) => { ImageCompleted?.Invoke(fileName); };
+                task = myWebClient.DownloadFileTaskAsync(remoteUri, fileName);
+            }
+        }
+    }
+}
